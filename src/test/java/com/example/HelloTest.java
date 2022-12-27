7a1a7ee44763c82a2ba9d9b3631b0fa2f773e681
pavithra-naagarajan <71398057+pavithra-naagarajan@users.noreply.github.com>
2022-12-27 12:14:01 +0530

    Update HelloTest.java

--- src/test/java/com/example/HelloTest.java
+++ src/test/java/com/example/HelloTest.java
@@ -14,7 +14,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HelloTest {
 
     @Test
-    public void evaluatesExpression() {
+    public void evaluatesExpression_1() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+     @Test
+    public void evaluatesExpression_2() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+     @Test
+    public void evaluatesExpression_3() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+     @Test
+    public void evaluatesExpression_4() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+     @Test
+    public void evaluatesExpression_5() {
         Hello hi = new Hello();
         int sum = hi.evaluate("1+2+3");
         assertEquals(9, sum);
