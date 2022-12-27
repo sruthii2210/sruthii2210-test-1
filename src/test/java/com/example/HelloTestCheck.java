38d7bbe9c17eb0fb024aa9615cce2971566ac627
pavithra-naagarajan <71398057+pavithra-naagarajan@users.noreply.github.com>
2022-12-27 12:15:06 +0530

    Create HelloTestCheck.java

---
+++ src/test/java/com/example/HelloTestCheck.java
@@ -0,0 +1,46 @@
+package com.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Unit test for Hello.
+ * <p/>
+ * A unit test aims to test all code and code paths of a specific class.
+ */
+public class HelloTestCheck {
+
+    @Test
+    public void evaluatesExpression_1() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+     @Test
+    public void evaluatesExpression_2() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+     @Test
+    public void evaluatesExpression_3() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+     @Test
+    public void evaluatesExpression_4() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+     @Test
+    public void evaluatesExpression_5() {
+        Hello hi = new Hello();
+        int sum = hi.evaluate("1+2+3");
+        assertEquals(9, sum);
+    }
+}
